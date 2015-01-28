13d4b73dfe07ac6c2b5557f543dfc7ad617ae440
Raymond Borkowski <rborkows@umich.edu>
2015-01-27 20:42:57 -0500

    Create dropTables.sql

---
+++ dropTables.sql
@@ -0,0 +1,15 @@
+DROP TABLE Hometown;
+DROP TABLE Current_loc;
+DROP TABLE School;
+DROP TABLE Friends;
+DROP TABLE Album;
+DROP TABLE Tag;
+DROP TABLE Comments;
+DROP TABLE Locs;
+DROP TABLE Message;
+DROP TABLE Particpates;
+DROP TABLE Studies;
+DROP TABLE Photo;
+DROP TABLE Location;
+DROP TABLE Users;
+DROP TABLE Events;
